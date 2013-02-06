5dbd676c29b8e863360f5078ef25aec4be44197b
Josue Camara <josue87c2@live.com.mx>
2013-02-05 22:02:21 -0600

    Nuevo metodo basicAuth

--- templates/libraries/jquery.basicAuth.js
+++ templates/libraries/jquery.basicAuth.js
@@ -1,33 +1,48 @@
 jQuery.basicAuth = (function($) {
 
-    methodLogin = function(url, code){
+    methodLogin = function(url, user,password) {
       
-      var result = false;
+      var result = {};
       var url = url;
-      var auth = "Basic "+code;
+      var string = user+':'+password;
+      var auth = "Basic "+$.base64.encode(string);
       $.ajax({
 	url:url,
+	async:false,
 	type: 'GET',//method: 'GET',
 	beforeSend: function(req) {
 	  req.setRequestHeader('Authorization',auth);
 	},
 	error: function(req, text, error) {
-	  result = false;
+	  result = {};
 	},
-	success: function(response){
-	  result = true;
+	success: function(response) {
+	  localStorage.basicAuth = auth.replace('Basic ','');
+	  result = {'response':response,'status':true};
 	}
       });
       return result;
    };
    
-   methodLogout = function(){
+   methodLogout = function() {
+     if(localStorage.removeItem("basicAuth")){
+       localStorage.removeItem("basicAuth");
+     }
      alert("Logout coming soon");
   };
    
+  methodHaveSession = function() {
+    var response = {};
+    if (localStorage.basicAuth) {
+      response = {'code':localStorage.basicAuth};
+    }
+    return response;
+  }
+  
   return {
     login: methodLogin,
-    logout: methodLogout
+    logout: methodLogout,
+    haveSession: methodHaveSession
   }
 
 }(jQuery));
