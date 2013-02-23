3f5ec442387bc707ef9b22bc976dd9ee342c1e02
julian <julian@pikhub.com>
2013-02-23 05:05:31 -0600

    dist added

--- dist/templates/app/init.js
+++
@@ -1,5 +0,0 @@
-(function() {
-
-  namespace(%(namespaceDeclaration)s);
-    
-})();
--- dist/templates/app/models.js
+++
@@ -1,7 +0,0 @@
-(function(namespace) {
-
-  var models = namespace.models;
-
-  //TODO: Add models below
-    
-})(%(namespace)s);
