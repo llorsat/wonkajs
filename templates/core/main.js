e3d62d3243423400c644f627c5d8960c92c4d452
Julian Ceballos <julian@pikhub.com>
2013-09-23 14:58:04 -0500

    appcache js removed

--- templates/core/main.js
+++ templates/core/main.js
@@ -1,13 +1,5 @@
 window.main = function() {
 
-  window.applicationCache.addEventListener('updateready', function(e) {
-    console.log('cache ready');
-    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-      window.applicationCache.swapCache();
-      window.location.reload();
-    }
-  }, false);
-
   Backbone.emulateJSON = true;
   
   Backbone.history.start();
